ce0b7352aaf851d01c4dc284b78bf1d39992e6b3
Adonis K <aklp08@gmail.com>
2013-03-23 18:56:46 +0200

    moved url parsing into loc.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -280,30 +280,6 @@ require(['jquery', 'appSession', 'loc'], function ($, appSession, loc) {
 	******************************************************* */
 		// TODO, document the url parsing
 
-	while (!! (loc.m = loc.regex.exec(loc.hash.string))) {
-		loc.hash.params[decodeURIComponent(loc.m[1])] = decodeURIComponent(loc.m[2]);
-	}
-
-	while (!! (loc.m = loc.regex.exec(loc.search.string))) {
-		loc.search.params[decodeURIComponent(loc.m[1])] = decodeURIComponent(loc.m[2]);
-	}
-
-	if(loc.hash.params.state === '?auth'){
-		localStorage.setItem('access_token', loc.hash.params.access_token);
-
-		$.get('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=' + localStorage.getItem('access_token'), function(data) {
-			if(parseInt(data.expires_in, 10) > 0 && data.audience === appSession.authenticate.client_id){
-				localStorage.setItem('expires_in', data.expires_in);
-				localStorage.setItem('access_type', data.access_type);
-				localStorage.setItem('user_id', data.user_id);
-
-				window.location.replace('./');
-			}else{
-				// TODO handle session hijacking or expiration errors
-			}
-		});
-	}
-
 	// if the query string is empty
 	if (loc.search.string === '') {
 		// then its the frontpage, so enable the default panels
