e4451ca07b0b0694ad88a2a8c6231ce120c44401
Adonis K <aklp08@gmail.com>
2013-12-26 13:13:56 +0200

    navigation semantics

--- src/assets/js/app/views/header.js
+++ src/assets/js/app/views/header.js
@@ -4,12 +4,12 @@ $(function () {
 	app.mvc.views.HeaderView = Backbone.View.extend({
 		el: '#container',
 		events: {
-			'click .navigating-btn': 'navigate',
+			// 'click .navigating-btn': 'navigate'
 		},
 		navigate: function (e) {
 			// TODO: validate id
-			var id = $(e.currentTarget).prop('id');
-			app.mvc.router.navigate(id, {trigger: true});
+			// var id = $(e.currentTarget).prop('id');
+			// app.mvc.router.navigate(id, {trigger: true});
 		},
 		initialize: function (params) {
 			this.render(params);
